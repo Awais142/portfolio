e67ab3b0633a77764ce8c55b43072cf823369370
Awais142 <awaisakbar142@gmail.com>
2024-10-07 03:29:18 +0500

    have fixed some styling in portfolio

--- src/Pages/Portfolio.jsx
+++ src/Pages/Portfolio.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import ShineBorder from "../Components/ui/shineBorder";
 import projects from "../lib/projects";
 import Slider from "react-slick";
-import { CSSTransition, TransitionGroup } from "react-transition-group";
+import { CSSTransition, SwitchTransition } from "react-transition-group";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
@@ -10,6 +10,7 @@ const Projects = () => {
   const [activeTab, setActiveTab] = useState("All");
   const [selectedProject, setSelectedProject] = useState(null);
   const [showModal, setShowModal] = useState(false);
+  const [prevTab, setPrevTab] = useState("All");
 
   const filteredProjects =
     activeTab === "All"
@@ -21,6 +22,8 @@ const Projects = () => {
             ? project.type === "Frontend"
             : activeTab === "Api"
             ? project.type === "Api"
+            : activeTab === "Wordpress"
+            ? project.type === "Wordpress"
             : true
         );
 
@@ -28,7 +31,15 @@ const Projects = () => {
     setSelectedProject(project);
     setShowModal(true);
   };
+
   const closeModal = () => setShowModal(false);
+
+  const getSlideDirection = () => {
+    const tabsOrder = ["All", "FullStack", "Frontend", "Api", "Wordpress"];
+    const prevIndex = tabsOrder.indexOf(prevTab);
+    const currentIndex = tabsOrder.indexOf(activeTab);
+    return currentIndex > prevIndex ? "slide-left" : "slide-right";
+  };
 
   return (
     <div className="relative text-gray-800 dark:text-gray-200 p-8">
@@ -37,7 +48,7 @@ const Projects = () => {
       </h1>
 
       <div className="flex justify-center mb-8">
-        {["All", "FullStack", "Frontend", "Api"].map((tab) => (
+        {["All", "FullStack", "Frontend", "Api", "Wordpress"].map((tab) => (
           <button
             key={tab}
             className={`relative px-4 py-2 rounded-lg mx-4 transition-all duration-300 ease-in-out transform ${
@@ -45,75 +56,98 @@ const Projects = () => {
                 ? "bg-rose-400 text-white scale-105 shadow-lg"
                 : "bg-gray-200 dark:bg-gray-700 hover:bg-gray-300 dark:hover:bg-gray-600"
             }`}
-            onClick={() => setActiveTab(tab)}
+            onClick={() => {
+              setPrevTab(activeTab);
+              setActiveTab(tab);
+            }}
           >
             {tab}
           </button>
         ))}
       </div>
 
-      <TransitionGroup className="relative flex justify-center flex-wrap gap-8">
-        <div className="absolute inset-0 h-full w-full bg-gradient-to-r from-orange-200 to-rose-200 transform scale-[0.80] blur-3xl"></div>
-        {filteredProjects.map((project, index) => (
-          <CSSTransition key={index} timeout={500} classNames="slide">
-            <ShineBorder
-              className="relative project-card flex flex-col items-center justify-between p-2 cursor-pointer rounded-lg bg-slate-100 dark:bg-gray-900 hover:scale-105 transition-transform duration-300 shadow-lg w-full max-w-xs h-full"
-              color={["#E96466", "#E74764", "#FFffff"]}
-              onClick={() => openModal(project)}
-            >
-              <div className="relative w-full h-60 overflow-hidden rounded-md">
-                <img
-                  src={project.image}
-                  alt={project.title}
-                  className="w-full h-full object-cover rounded-md transition-all duration-500 hover:blur-sm"
-                />
-                <div className="absolute inset-0 flex flex-col items-center justify-center bg-black bg-opacity-50 text-white opacity-0 hover:opacity-100 transition-opacity duration-500">
-                  <h3 className="text-lg font-bold">{project.title}</h3>
-                  <button
-                    className="mt-2 text-indigo-400 underline"
-                    onClick={() => openModal(project)}
-                  >
-                    Learn More
-                  </button>
-                </div>
+      <SwitchTransition>
+        <CSSTransition
+          key={activeTab}
+          timeout={500}
+          classNames={getSlideDirection()}
+        >
+          <div className="relative flex justify-center flex-wrap gap-8 transition-all duration-500">
+            {filteredProjects.map((project, index) => (
+              <div className="relative project-card flex flex-col items-center justify-between w-full max-w-xs h-full">
+                {/* Backdrop Effect */}
+                <div className="absolute inset-0 h-full w-full bg-gradient-to-r from-orange-300 to-rose-400 transform scale-[0.80] rounded-full blur-3xl"></div>
+
+                <ShineBorder
+                  key={index}
+                  className="relative flex flex-col items-center justify-between p-2 cursor-pointer rounded-lg bg-slate-100 dark:bg-gray-900 hover:scale-105 transition-transform duration-300 shadow-lg"
+                  color={["#E96466", "#E74764", "#FFffff"]}
+                  onClick={() => openModal(project)}
+                >
+                  <div className="relative w-full h-60 overflow-hidden rounded-md">
+                    <img
+                      src={project.image}
+                      alt={project.title}
+                      className="w-full h-full object-cover rounded-md transition-all duration-500 hover:blur-sm"
+                    />
+                    <div className="absolute inset-0 flex flex-col items-center justify-center bg-black bg-opacity-50 text-white opacity-0 hover:opacity-100 transition-opacity duration-500">
+                      <h3 className="text-lg font-bold">{project.title}</h3>
+                      <button
+                        className="mt-2 text-indigo-400 underline"
+                        onClick={() => openModal(project)}
+                      >
+                        Learn More
+                      </button>
+                    </div>
+                  </div>
+                </ShineBorder>
               </div>
-            </ShineBorder>
-          </CSSTransition>
-        ))}
-      </TransitionGroup>
+            ))}
+          </div>
+        </CSSTransition>
+      </SwitchTransition>
 
       {showModal && selectedProject && (
         <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
-          <div className="bg-white dark:bg-gray-800 p-6 rounded-lg shadow-lg w-full max-w-3xl">
+          <div className="bg-white dark:bg-gray-800 p-6 rounded-lg shadow-lg w-full max-w-3xl relative">
             <button
               className="absolute top-2 right-2 text-xl font-bold text-red-500"
               onClick={closeModal}
             >
               &times;
             </button>
-            <h2 className="text-3xl font-bold mb-4">{selectedProject.title}</h2>
-            <p className="mb-4">{selectedProject.description}</p>
+            <div className="modal-content">
+              <img
+                src={selectedProject.images[0]}
+                alt={selectedProject.title}
+                className="w-full h-64 object-cover rounded-md mb-4"
+              />
+              <h2 className="text-3xl font-bold mb-4">
+                {selectedProject.title}
+              </h2>
+              <p className="mb-4">{selectedProject.description}</p>
 
-            {selectedProject.images && selectedProject.images.length > 0 && (
-              <Slider>
-                {selectedProject.images.map((img, index) => (
-                  <img
-                    key={index}
-                    src={img}
-                    alt={selectedProject.title}
-                    className="w-full h-64 object-cover rounded-md"
-                  />
-                ))}
-              </Slider>
-            )}
+              {selectedProject.images && selectedProject.images.length > 1 && (
+                <Slider>
+                  {selectedProject.images.slice(1).map((img, index) => (
+                    <img
+                      key={index}
+                      src={img}
+                      alt={selectedProject.title}
+                      className="w-full h-64 object-cover rounded-md"
+                    />
+                  ))}
+                </Slider>
+              )}
 
-            <div className="mt-4">
-              <h3 className="text-xl font-semibold">Skills Used:</h3>
-              <ul className="list-disc ml-4">
-                {selectedProject.skills.map((skill, index) => (
-                  <li key={index}>{skill}</li>
-                ))}
-              </ul>
+              <div className="mt-4">
+                <h3 className="text-xl font-semibold">Skills Used:</h3>
+                <ul className="list-disc ml-4">
+                  {selectedProject.skills.map((skill, index) => (
+                    <li key={index}>{skill}</li>
+                  ))}
+                </ul>
+              </div>
             </div>
           </div>
         </div>
